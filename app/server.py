aca4674fc1e634ea27a08bc571091c5ec8c80e6d
DanielOyeniyi <61523788+DanielOyeniyi@users.noreply.github.com>
2020-02-29 00:09:35 -0800

    Add files via upload

--- app/server.py
+++ app/server.py
@@ -1,7 +1,6 @@
 import json
 import os
 import random
-import Logic
 
 import bottle
 from bottle import HTTPResponse
@@ -9,7 +8,7 @@ from bottle import HTTPResponse
 
 @bottle.route("/")
 def index():
-    return "Your Battlesnake is alive! or is it?"
+    return "Your Battlesnake is alive!"
 
 
 @bottle.post("/ping")
@@ -61,7 +60,7 @@ def move():
     shout = "I am a python snake!"
 
     #response = {"move": "right", "shout": shout}
-    response = Logic.next_move(data)
+    response = next_move(data)
     return HTTPResponse(
         status=200,
         headers={"Content-Type": "application/json"},
@@ -74,6 +73,84 @@ def move():
 # takes a dict representing the board data
 # then returns a move response
 #def dicision(dict)
+
+# (dict) -> str
+# takes a dict containg board info and produces a direction
+# turn is a int
+# you is a dict
+# board is a dict
+# body is a list of dicts
+# head is a dict representing x,y coordinates
+def next_move(data):
+    turn = data["turn"]
+    you = data["you"]
+    board = data["board"]
+    body = you["body"]
+    head = body[0] 
+    range = board["height"]
+    range -= 1 # we start counting at 0
+    
+    # directions = ["up", "down", "left", "right"]
+    # bad_snake(directions, body)
+    
+    # uhhh... the snake will colide into itself depending on where it's body is
+    # are a bunch or if else statements all we need? or can we implement recursion
+    # mabey blocks of moves to fill a small 4 by 4 grid, but what about other snakes? 
+    # or if your body size is bigger than 3
+    
+    
+    if (head["x"] == 0 and head["y"] == 0):            # top left corner 
+        directions = ["down", "right"]
+        move = random.choice(directions)
+        return move
+        
+    elif (head["x"] == range and head["y"] == 0):      # top right corner 
+        directions = ["down", "left"]
+        move = random.choice(directions)
+        return move
+        
+    elif (head["x"] == range and head["y"] == range):  # bottom right corner 
+        directions = ["up", "left"]
+        move = random.choice(directions)
+        return move
+        
+    elif (head["x"] == 0 and head["y"] == range):      # bottom left corner
+        directions = ["up", "right"]
+        move = random.choice(directions)
+        return move
+
+    elif (head["x"] == 0):                             # left wall
+        directions = ["up", "down","right"]
+        move = random.choice(directions)
+        return move
+        
+    elif (head["y"] == 0):                             # top wall 
+        directions = ["down", "left","right"]
+        move = random.choice(directions)
+        return move
+
+    elif (head["x"] == range):                         # right wall 
+        directions = ["up", "down","left"]
+        move = random.choice(directions)
+        return move
+        
+    elif (head["y"] == range):                         # bottom wall
+        directions = ["up", "left","right"]
+        move = random.choice(directions)
+        return move
+
+    else:
+        directions = ["up", "down", "left", "right"]   # middle of board
+        move = random.choice(directions)
+        return move
+    
+    
+
+
+
+
+
+
 
 
 @bottle.post("/end")
