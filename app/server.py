23b59ce49aaeb20eea79ad7440be27ce8b1cdfc6
DanielOyeniyi <61523788+DanielOyeniyi@users.noreply.github.com>
2020-02-29 21:16:35 -0800

    Add files via upload

--- app/server.py
+++ app/server.py
@@ -61,35 +61,21 @@ def move():
     # create seperate files classes
     # (0,0) is top left
     
-# (dict) -> string
-# takes a dict representing the board data
-# then returns a move response
-#def dicision(dict)
 
 # dict -> string
 # takes a dict containg board info and produces a direction
 def next_move(data):
-    turn = data["turn"]                 # turn is a int representign the turn in game
-    you = data["you"]                   # you is a dict representing your snakes data
-    board = data["board"]               # board is a dict representing the game board info
-    snakes = occupied(board["snakes"])  # locations of occupied spots on the board
-    food = board["food"]                # locations of food on the board
-    body = you["body"]                  # body is a list of dicts representing your snakes location
-    health = you["health"]               # health of the snake
-    head = body[0]                      # head is a dict representing your snakes head
-    max = board["height"]               # max is the dimention number e.g. 14 by 14
-    max -= 1                            # we start counting at 0 because we are coders
-    
-    # directions = ["up", "down", "left", "right"]
-    # bad_snake(directions, body)
-    
-    # uhhh... the snake will colide into itself depending on where it's body is
-    # are a bunch or if else statements all we need? or can we implement recursion
-    # mabey blocks of moves to fill a small 4 by 4 grid, but what about other snakes? 
-    # initial size is 3
-    # is a stack a good use here? 
-    # if you have a head on head collision the smaller snake dies
-    
+    turn = data["turn"]                      # turn is a int representign the turn in game
+    you = data["you"]                        # you is a dict representing your snakes data
+    board = data["board"]                    # board is a dict representing the game board info
+    snakes = board["snakes"]                 # locations of occupied spots on the board
+    food = board["food"]                     # locations of food on the board
+    body = you["body"]                       # body is a list of dicts representing your snakes location
+    health = you["health"]                   # health of the snake
+    head = body[0]                           # head is a dict representing your snakes head
+    max = board["height"]                    # max is the dimention number e.g. 14 by 14
+    max -= 1                                 # we start counting at 0 because we are coders
+ 
     
     if (head["x"] == 0 and head["y"] == 0):            # top left corner 
         directions = ["down", "right"]
@@ -158,32 +144,37 @@ def eat_food(lod, food, body, health):
 
 # list -> list
 # makes a list of all the occupied spots on the map
-def occupied(snakes):
+def make_occupied(snakes):
     occupied = []
     for snake in snakes:
         body = snake["body"]
         for block in body: 
             occupied.append(block)
     return occupied
+    
+def make_tails(snakes): 
+    tails = []
+    for snake in snakes: 
+        body = snake["body"]
+        tails.append(body[-1])
+    return tails
 
-# we want our code to now go after food
 
 # list, list, int -> string
 # should sense the possible options and pick the ones
 # that won't result in instant death
 def body_sensor(lod, body, snakes, max):
     head = body[0]
-    tail = body[len(body)-1]
     headx = head["x"]
     heady = head["y"]
+    tails = make_tails(snakes)
+    snakes = make_occupied(snakes)
     right_block = {"x": headx+1, "y": heady}    
     left_block = {"x": headx-1, "y": heady}      
     down_block = {"x": headx, "y": heady+1}   
     up_block = {"x": headx, "y": heady-1}    
     
     
-    # would be way more efficient if you just kept a dict of all the 
-    # occupied locations on the board
 
     if (right_block in snakes and "right" in lod):  
         lod.remove("right")
@@ -202,8 +193,8 @@ def body_sensor(lod, body, snakes, max):
             block1 = block_picker(lod[0], right_block, left_block, down_block, up_block)
             block2 = block_picker(lod[1], right_block, left_block, down_block, up_block)
             
-            choice1 = advanced_body_sensor(block1, snakes, tail, max)
-            choice2 = advanced_body_sensor(block2, snakes, tail, max)
+            choice1 = advanced_body_sensor(block1, snakes, tails, max)
+            choice2 = advanced_body_sensor(block2, snakes, tails, max)
             
             if (choice1 > choice2):
                 del lod[1]
@@ -219,9 +210,9 @@ def body_sensor(lod, body, snakes, max):
             block2 = block_picker(lod[1], right_block, left_block, down_block, up_block)
             block3 = block_picker(lod[2], right_block, left_block, down_block, up_block)
             
-            choice1 = advanced_body_sensor(block1, snakes, tail, max)
-            choice2 = advanced_body_sensor(block2, snakes, tail, max)
-            choice3 = advanced_body_sensor(block3, snakes, tail, max)
+            choice1 = advanced_body_sensor(block1, snakes, tails, max)
+            choice2 = advanced_body_sensor(block2, snakes, tails, max)
+            choice3 = advanced_body_sensor(block3, snakes, tails, max)
             
             if (choice1 > choice2 and choice1 > choice3):     # choice1 is biggest
                 del lod[2]
@@ -261,7 +252,7 @@ def body_sensor(lod, body, snakes, max):
 # dict, dict, int -> int
 # takes the block and returns the # of options
 # the snake has in this block
-def advanced_body_sensor(block, snakes, tail, max):  # can we make it check even more possibilities?
+def advanced_body_sensor(block, snakes, tails, max):  # can we make it check even more possibilities?
     blockx = block["x"]
     blocky = block["y"]
     right_block = {"x": blockx+1, "y": blocky} 
@@ -270,6 +261,8 @@ def advanced_body_sensor(block, snakes, tail, max):  # can we make it check even
     up_block = {"x": blockx, "y": blocky-1} 
     count = 0                                 # count of available blocks                  
     
+    # how to make it ignore the tail of other snakes? 
+    # what about the head? diagonal?
 
     if (right_block not in snakes):
         count += 1       
@@ -280,14 +273,15 @@ def advanced_body_sensor(block, snakes, tail, max):  # can we make it check even
     if (up_block not in snakes): 
         count += 1
     
-    if (right_block == tail):
-        count += 1
-    if (left_block == tail):
-        count += 1
-    if (down_block == tail):
-        count += 1
-    if (up_block == tail):
-        count += 1
+    for tail in tails:
+        if (right_block == tail):
+            count += 1
+        if (left_block == tail):
+            count += 1
+        if (down_block == tail):
+            count += 1
+        if (up_block == tail):
+            count += 1
     
 
     if (blockx == 0 or blockx == max):
