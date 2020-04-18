0b758fb6619c3f2c1cf16bb6b07081c8d7c57881
DanielOyeniyi <61523788+DanielOyeniyi@users.noreply.github.com>
2020-04-18 00:32:20 -0600

    Add files via upload

--- app/server.py
+++ app/server.py
@@ -108,7 +108,58 @@ def move():
 
     #END OF FIRST PRIORITY #############################################
 
-        #SECOND PRIORITY - Snake should find shortest path to fruit
+
+        #SECOND PRIORITY - Snake should avoid hitting itself
+
+    # locHeadX + 1  checks right block of head  --> if blocked: up
+    # locHeadX - 1  checks left block of head --> if blocked:right
+    # locHeadY + 1  checks top block of head --> if blocked: down
+     # locHeadY - 1  checks bottom block of head --> if blocked: left
+
+    # sample if statement: if dictionary_location_of_body = location_listed_above --> make_the_move_as_Defined_above  //so if snake body is 1 left of head, move right
+
+    # go through all the body parts in the data and compare the x and y values of each body part in a snake
+
+    right = {"x": locHeadX + 1, "y": locHeadY} 
+    if (right in data["you"]["body"]):  #checks to see if body part is 1 right to the head.
+      move = "up"
+      
+    left = {"x": locHeadX - 1, "y": locHeadY} 
+    if (left in data["you"]["body"]):  #checks to see if body part is 1 left to the head.
+      move = "down"
+      
+    up = {"x": locHeadX, "y": locHeadY-1}
+    down = {"x": locHeadX, "y": locHeadY+1}
+          
+    if (up in data["you"]["body"]): #checks to see if body part is 1 up to the head.
+        move = "right"
+        
+    if (down in data["you"]["body"]): #checks to see if body part is 1 down to the head.
+        move = "left"
+      
+      
+    '''
+    if (data["you"]["body"][i]["x"] == (locHeadX + 1) and (data["you"]["body"][i]["y"] == locHeadY):  #checks to see if body part is 1 right to the head.
+        move = "up"
+    elif (data["you"]["body"][i]["x"] == (locHeadX - 1) and (data["you"]["body"][i]["y"] == locHeadY): #checks to see if body part is 1 left to the head.
+        move = "down"
+    elif (data["you"]["body"][i]["x"] == (locHeadX) and (data["you"]["body"][i]["y"] == (locHeadY + 1)): #checks to see if body part is 1 up to the head.
+            move = "right"
+    elif (data["you"]["body"][i]["x"] == (locHeadX) and (data["you"]["body"][i]["y"] == (locHeadY - 1)): #checks to see if body part is 1 down to the head.
+            move = "left"         
+    '''
+          
+
+        
+
+    #END OF SECOND PRIORITY #############################################
+
+
+
+
+
+
+    #THIRD PRIORITY - Snake should find shortest path to fruit
 
 
 
@@ -116,7 +167,7 @@ def move():
 
 
 
-
+    #END OF THIRD PRIORITY #############################################
 
 
 
@@ -131,6 +182,7 @@ def move():
         headers={"Content-Type": "application/json"},
         body=json.dumps(response),
     )
+
 
 
      
