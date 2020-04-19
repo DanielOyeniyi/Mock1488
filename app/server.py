94e30cbf7662b8866b2e139debcdf6fac2adff51
DanielOyeniyi <61523788+DanielOyeniyi@users.noreply.github.com>
2020-04-18 18:55:09 -0600

    Add files via upload

--- app/server.py
+++ app/server.py
@@ -44,137 +44,14 @@ def move():
     Your response must include your move of up, down, left, or right.
     """
     data = bottle.request.json
-    #next_move(data)
+    next_move(data)
     print("MOVE:", json.dumps(data))
 
-    moves = ["right", "left", "down", "up"]
-    move = random.choice(moves) #Notmove > halfWidth to centerB x and Notmove > halfWidth to centerB y
-
-    #move = next_move(data)
-
-    #FIRST PRIORITY - Snake should avoid hitting walls 
-    heightBoard = data["board"]["height"] #retrieve height 
-    widthBoard = data["board"]["width"]  #retrieve width 
-
-    halfHeight = heightBoard / 2
-    halfWidth = widthBoard / 2
-
-    centerBlock = {		#location of center of board
-      "centerB":
-        {
-          "x": halfWidth,
-          "y": halfHeight
-        }  
-    }
-
-          # LOCATION: of snake head
-    locHeadX = data["you"]["body"][0]["x"]
-    locHeadY = data["you"]["body"][0]["y"]
-
-    #If statement to force snake away from boundaries/wall
-    #x boundary/walls/corners
-    if (locHeadX == (widthBoard-1)):  #farthest x-axis length, a length of 11 is represented by index 10  
-          # LOCATION: of neck1 
-        neck1X = data["you"]["body"][1]["x"]
-        if(neck1X ==(widthBoard-2)):   #if body is in the left column next to the farthest length value
-            move = "up"
-        else:
-            move = "left"
-
-    if (locHeadX == 0):  #closest x-axis length
-
-        neck1X = data["you"]["body"][1]["x"]
-        if(neck1X ==(1)):
-          move = "up"
-        else:
-          move = "right"
-
-
-    #y boundary/walls/corners
-    if (locHeadY == heightBoard):
-        neck1Y = data["you"]["body"][1]["y"]
-        if (neck1Y == heightBoard-2):
-            move = "right"
-        else:
-            move = "up"
-
-    if (locHeadY == 0):
-        neck1Y = data["you"]["body"][1]["y"]
-        if (neck1Y == 1):
-          move = "right"
-        else:
-          move = "down"
-
-
-    #END OF FIRST PRIORITY #############################################
-
-
-        #SECOND PRIORITY - Snake should avoid hitting itself
-
-    # locHeadX + 1  checks right block of head  --> if blocked: up
-    # locHeadX - 1  checks left block of head --> if blocked:right
-    # locHeadY + 1  checks top block of head --> if blocked: down
-     # locHeadY - 1  checks bottom block of head --> if blocked: left
-
-    # sample if statement: if dictionary_location_of_body = location_listed_above --> make_the_move_as_Defined_above  //so if snake body is 1 left of head, move right
-
-    # go through all the body parts in the data and compare the x and y values of each body part in a snake
-
-    right = {"x": locHeadX + 1, "y": locHeadY} 
-    if (right in data["you"]["body"]):  #checks to see if body part is 1 right to the head.
-      move = "up"
-      
-    left = {"x": locHeadX - 1, "y": locHeadY} 
-    if (left in data["you"]["body"]):  #checks to see if body part is 1 left to the head.
-      move = "down"
-      
-    up = {"x": locHeadX, "y": locHeadY-1}
-    down = {"x": locHeadX, "y": locHeadY+1}
-          
-    if (up in data["you"]["body"]): #checks to see if body part is 1 up to the head.
-        move = "right"
-        
-    if (down in data["you"]["body"]): #checks to see if body part is 1 down to the head.
-        move = "left"
-      
-      
-    '''
-    if (data["you"]["body"][i]["x"] == (locHeadX + 1) and (data["you"]["body"][i]["y"] == locHeadY):  #checks to see if body part is 1 right to the head.
-        move = "up"
-    elif (data["you"]["body"][i]["x"] == (locHeadX - 1) and (data["you"]["body"][i]["y"] == locHeadY): #checks to see if body part is 1 left to the head.
-        move = "down"
-    elif (data["you"]["body"][i]["x"] == (locHeadX) and (data["you"]["body"][i]["y"] == (locHeadY + 1)): #checks to see if body part is 1 up to the head.
-            move = "right"
-    elif (data["you"]["body"][i]["x"] == (locHeadX) and (data["you"]["body"][i]["y"] == (locHeadY - 1)): #checks to see if body part is 1 down to the head.
-            move = "left"         
-    '''
-          
-
-        
-
-    #END OF SECOND PRIORITY #############################################
-
-
-
-
-
-
-    #THIRD PRIORITY - Snake should find shortest path to fruit
-
-
-
-
-
-
-
-    #END OF THIRD PRIORITY #############################################
-
-
-
+    move = next_move(data)
+    
     # Shouts are messages sent to all the other snakes in the game.
     # Shouts are not displayed on the game board.
     shout = "I am a python snake!"
-        
 
     response = {"move": move, "shout": shout}
     return HTTPResponse(
@@ -184,8 +61,122 @@ def move():
     )
 
 
+num_loops = 0
 
-     
+# we want to map out all the possible moves. recursion 
+# sounds like a great way to do it
+# first take into account all of our moves then 
+# we take into account the possible moves of other snakes
+def next_move(data):
+    global num_loops
+    num_loops = 0
+    return value(data)
+
+
+# we could do that check every available spot with the next 6 moves
+# but the sizing would have to change as that would take too 
+# long to compute
+
+# dict, dict -> string
+# function checks all the moves up to a certain depth
+def value(data):
+    head = data["you"]["body"][0]
+    directions = []
+    
+    right_block = {"x": head["x"] + 1, "y": head["y"]}
+    left_block = {"x": head["x"] - 1, "y": head["y"]}
+    down_block = {"x": head["x"], "y": head["y"] + 1}
+    up_block = {"x": head["x"], "y": head["y"] - 1}
+    
+    right_val = 0
+    left_val = 0
+    down_val = 0
+    up_val = 0
+    
+    right_val = value_helper(data, 0, right_block)
+    left_val = value_helper(data, 0, left_block)
+    down_val = value_helper(data, 0, down_block)
+    up_val = value_helper(data, 0, up_block)
+    
+    
+    print(num_loops)
+    max_val = max(right_val, left_val, down_val, up_val)
+    if (max_val == right_val):
+        directions.append("right")
+    if (max_val == left_val):
+        directions.append("left")
+    if (max_val == down_val):
+        directions.append("down")
+    if (max_val == up_val):
+        directions.append("up")
+    return random.choice(directions)
+
+# this is super inneficient
+# currently it's just checking available blocks rather than all the available 
+# blocks of previous snakes
+# for enemy snake moves we can call this but 
+# add a bool that determins if numberes will be added or
+# subtracted
+def value_helper(data, depth, block):
+    global num_loops 
+    num_loops += 1
+    if (depth == 8 or not is_available(data, block)):
+        return 0
+    else:
+        right_block = {"x": block["x"] + 1, "y": block["y"]}
+        left_block = {"x": block["x"] - 1, "y": block["y"]}
+        down_block = {"x": block["x"], "y": block["y"] + 1}
+        up_block = {"x": block["x"], "y": block["y"] - 1}
+        
+        right_val = 0
+        left_val = 0
+        down_val = 0
+        up_val = 0
+        
+        
+        # think of proper score calculations, i.e if you can kill a snake
+        # then that is worth more than just surviving, food score 
+        # should be considered too
+        
+        right_val = value_helper(data, depth+1, right_block)
+        left_val = value_helper(data, depth+1, left_block)
+        down_val = value_helper(data, depth+1, down_block)
+        up_val = value_helper(data, depth+1, up_block)
+            
+        if (right_block in data["board"]["food"]):
+            right_val += 1
+        if (left_block in data["board"]["food"]):
+            left_val += 1
+        if (down_block in data["board"]["food"]):
+            down_val += 1
+        if (up_block in data["board"]["food"]):
+            up_val += 1
+            
+        max_val = max(right_val, left_val, down_val, up_val)
+            
+        return max_val + 1
+
+
+# to save computing power rather than check if the
+# block is in snakes or a wall everytime why not 
+# have a is_near function that runs instead?
+# but how much computing power does that really save?
+
+# dict, dict -> bool
+# takes game board and a dict with x,y coordinates 
+# then returns a bool corresponding to the coordinates 
+# location on the board. i.e. false if pos is in snakes 
+# or a wall, false otherwise
+def is_available(data, pos):
+    for snake in data["board"]["snakes"]:
+        if (pos in snake["body"]):
+            return False
+    if (pos["x"] == -1 or pos["x"] == data["board"]["width"]):
+        return False
+    if (pos["y"] == -1 or pos["y"] == data["board"]["width"]):
+        return False
+    return True
+    
 @bottle.post("/end")
 def end():
     """
